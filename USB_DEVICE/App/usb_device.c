ca28a27d3bae91269322f59591763a0f9f5e7420
caijie <caijie@cuav.net>
2020-08-04 17:23:55 +0800

    成功枚举

--- USB_DEVICE/App/usb_device.c
+++ USB_DEVICE/App/usb_device.c
@@ -28,7 +28,7 @@
 #include "usbd_storage_if.h"
 
 /* USER CODE BEGIN Includes */
-
+#include "usbd_mc.h"
 /* USER CODE END Includes */
 
 /* USER CODE BEGIN PV */
@@ -73,11 +73,15 @@ void MX_USB_DEVICE_Init(void)
   {
     Error_Handler();
   }
-  if (USBD_RegisterClass(&hUsbDeviceHS, &USBD_MSC) != USBD_OK)
-  {
-    Error_Handler();
-  }
-  if (USBD_MSC_RegisterStorage(&hUsbDeviceHS, &USBD_Storage_Interface_fops_HS) != USBD_OK)
+//  if (USBD_RegisterClass(&hUsbDeviceHS, &USBD_MSC) != USBD_OK)
+//  {
+//    Error_Handler();
+//  }
+//  if (USBD_MSC_RegisterStorage(&hUsbDeviceHS, &USBD_Storage_Interface_fops_HS) != USBD_OK)
+//  {
+//    Error_Handler();
+//  }
+  if (USBD_RegisterClass(&hUsbDeviceHS, &USBD_MC) != USBD_OK)
   {
     Error_Handler();
   }
